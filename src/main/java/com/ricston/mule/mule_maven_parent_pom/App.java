b9bbf46941c3a508114a24c420bb890b4bdbe6ab
Bassem Elsodany <bassem.elsodany@hotmail.com>
2019-09-02 15:32:49 +0100

    first commit

---
+++ src/main/java/com/ricston/mule/mule_maven_parent_pom/App.java
@@ -0,0 +1,13 @@
+package com.ricston.mule.mule_maven_parent_pom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
